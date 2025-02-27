294a5fd71e87eb9c5bbac9023565cc195f7cb747
jewook-an <arch9503@gmail.com>
2025-02-27 13:39:14 +0900

    config 정리

--- config.py
+++ config.py
@@ -1,11 +1,4 @@
-"""
-NEWS_WEBSITE_URL = "https://example-news-website.com"
-SLEEP_TIME = 3
-OUTPUT_CSV = "news_headlines.csv"   # 별도 입력 받아 처리리
-LOG_FILE = "scraper.log"
-"""
-
-NEWS_WEBSITE_URL = "https://www.spotvnews.co.kr/"
+# NEWS_WEBSITE_URL = "https://www.spotvnews.co.kr/"
 SLEEP_TIME = 3
 LOG_FILE = "scraper.log"
 
